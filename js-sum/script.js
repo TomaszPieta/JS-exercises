55a718c966ab2ce3afc860cfe46f1df36ea7614f
Thompson1981 <chronos1981>
2018-04-03 09:59:57 +0200

    commit

---
+++ js-sum/script.js
@@ -0,0 +1,8 @@
+﻿var zmienna1 = 12;
+var zmienna2 = 16;
+var zmienna3 = zmienna1+zmienna2;
+function wypisz(){
+  document.getElementById("wstaw1").innerHTML = "Zmienna pierwsza to "+zmienna1;
+  document.getElementById("wstaw2").innerHTML = "Zmienna druga to "+zmienna2;
+  document.getElementById("wstaw3").innerHTML = "Suma dwoch zmiennych to "+zmienna3;
+}
