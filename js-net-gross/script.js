901fb156e95eb57de045f2d05abca39e18aa0556
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-04-09 21:00:42 +0200

    Delete script.js

--- js-net-gross/script.js
+++
@@ -1,6 +0,0 @@
-function przelicz(kwota){
-    return 1.23*kwota}
-
-var kwota = prompt("podaj wartość");
-
-document.getElementById("span").innerHTML = "Cena po dodaniu podatku 23% wynosi " + przelicz(kwota) + " złotych.";
