51ae06f9669919e7f59203d2c7f7ea84a9346656
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-04-18 10:34:42 +0200

    commit

---
+++ js-dog-talk/script.js
@@ -0,0 +1,19 @@
+﻿let dog = {
+  sound: 'woof',
+  talk: function(){
+    console.log(this.sound);
+    document.getElementById("spot").innerHTML = this.sound;
+  }}
+
+
+/*let talkFunction = dog.talk
+let boundFunction = talkFunction.bind(dog)
+talkFunction()
+boundFunction()
+let boundedFunction = dog.talk.bind(dog)
+boundedFunction()*/
+
+let button = document.getElementById('myButton')
+button.addEventListener('click', dog.talk.bind(dog))
+
+
