776eaa70b52af63f3481581de558487bc966523b
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-05-11 11:22:59 +0200

    commit

---
+++ kyu-4-sum-strings/readme.md
@@ -0,0 +1,1 @@
+Given the string representations of two integers, return the string representation of the sum of those integers
---
+++ kyu-4-sum-strings/script.js
@@ -0,0 +1,4 @@
+﻿function sumStrings(a,b){
+    let numbers = (parseInt(a)+parseInt(b)).toString();
+    console.log(numbers);}
+  sumStrings('1','3');
