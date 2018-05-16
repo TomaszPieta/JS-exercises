7062551f615db8966e2a090b113fa0326207138e
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-05-16 09:50:26 +0200

    commit

---
+++ kyu-6-spin-words/script.js
@@ -0,0 +1,14 @@
+﻿function spinWord(word){
+  let letters = word.split(' ');
+  let sentence = [];
+  letters.map((letter)=>{
+    if (letter.length>4){
+    list = letter.split('').reverse().join('');
+      sentence.push(list);
+  } else {
+  sentence.push(letter);}
+  return sentence.join('');
+  console.log(sentence);
+  });  
+}
+spinWord('moje paranoje swoje');
