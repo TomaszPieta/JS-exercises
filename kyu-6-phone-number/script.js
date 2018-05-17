bd72d385dce9de19874b9f399d9a73207082d0f1
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-05-17 13:08:53 +0200

    commit

---
+++ kyu-6-phone-number/script.js
@@ -0,0 +1,6 @@
+﻿function num(number){
+  let num = number.toString();
+  let phone = `(${num[0]}${num[1]}${num[2]}) ${num[3]}${num[4]}${num[5]}-${num[6]}${num[7]}${num[8]}${num[9]}`;
+  console.log(phone);
+}
+num(1234567890);
