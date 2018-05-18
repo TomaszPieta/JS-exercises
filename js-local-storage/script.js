1617b8fc9826376be4a5f7b3249eebd0c330bc9c
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-05-18 11:30:35 +0200

    commit

---
+++ js-local-storage/script.js
@@ -0,0 +1,14 @@
+﻿let name = document.forms.form.name.value;
+let salary = document.forms.form.salary.value;
+let data = document.forms.form.data.value;
+
+save = () => localStorage.setItem( name, salary );
+load = () => salary = localStorage.getItem( name );
+delete = () => data = localStorage.getItem( name );
+show = () => {
+  let box = `Found ${localStorage.length} people:\r\n`;
+  for (let id in localStorage) {
+    box += `${id} localStorage.getItem(id) \r\n`
+  }
+  alert(box);
+}
