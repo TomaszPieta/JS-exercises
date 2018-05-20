1d122fb589ce977f4fb1dbc0d506fa07efb32ef1
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-05-20 20:09:23 +0200

    commit

---
+++ js-function-time/script.js
@@ -0,0 +1,16 @@
+﻿function time(){
+	const event = (
+  	<div>
+  	  <h1>Current Time:</h1>
+      <h2>{new Date().toLocaleTimeString()}</h2>
+  	</div>
+  );
+  ReactDOM.render(
+  	event,
+    document.getElementById("root")
+  );
+}
+setInterval(time, 1000);
+
+
+
