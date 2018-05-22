51102897b8e1e0b1686e8b6142e27ae865b3b7b4
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-05-22 11:58:45 +0200

    commit

---
+++ js-typescript-timing/script.ts
@@ -0,0 +1,25 @@
+﻿class Greeter {
+  element: HTMLElement;
+  span: HTMLElement;
+  timerToken: number;
+  
+  constructor(element: HTMLElement) {
+    this.element = element;
+    this.element.innerText += "The time is: ";
+    this.span = document.createElement('span');
+    this.span.innerText = new Date().toUTCString();
+  }
+  start(){
+    this.timerToken = setInterval( () => {
+      this.span.innerText = new Date().toUTCString();
+    }, 500 );
+  }
+  stop(){
+    clearInterval(this.timerToken);
+  }
+}
+window.onload = () => {
+    var el = document.getElementById('content');
+    var greeter = new Greeter(el);
+    greeter.start();
+};
