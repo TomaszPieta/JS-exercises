b683d8652ab90315f7f226a28c88f06ad2fce47f
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-05-23 13:12:23 +0200

    commit

---
+++ js-country-finder/script.js
@@ -0,0 +1,22 @@
+﻿$( () => {
+    let url = 'https://restcountries.eu/rest/v1/name/';
+    let list = $('#countries');
+    let button = $('#search').click(search);
+    function search() {
+      let name = $('#name').val();
+      if (!name.length) {
+        $('<p>').text('Enter country name.').appendTo(list).css('color', 'red');
+      }
+      $.ajax({
+        method: 'GET',
+        url: url + name,
+        success: show
+      });
+    }
+    function show(response) {
+      list.empty();
+      response.forEach( (item) => {
+        $('<li>').text(`You have been looking for: ${item.name}, capital: ${item.capital} population: ${item.population}, area: ${item.area}`).appendTo(list);
+      });
+    }
+  });
