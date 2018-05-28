f21e736f4db5a44904d554f69e74d8664059a6ae
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-05-28 21:28:29 +0200

    commit

---
+++ js-reksio/script.js
@@ -0,0 +1,19 @@
+﻿class Animal {  
+  constructor() {}
+  giveVoice() {
+      console.log(`Nazywam sie ${this.name}!`);
+  }
+}
+class Dog extends Animal {
+constructor() {
+  super(name);
+}
+giveVoice() {    
+  super.giveVoice();
+}
+eat() {
+  console.log('yummy');
+}
+}
+const pies = new Dog('reksio');  
+pies.giveVoice();
