286bb7974a7be30e533aa9df1ae66f4c4a355724
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-06-21 17:37:00 +0200

    commit

---
+++ js-drag-drop-observable/script.js
@@ -0,0 +1,17 @@
+﻿let parent = document.getElementById('parent');
+let widget = document.getElementById('widget');
+let mouseDowns = Observable.fromEvent(widget, 'mousedown');
+let parentMouseMoves = Observable.fromEvent(parent, 'mousemove');
+let parentMouseMoveUps = Observable.fromEvent(parent, 'mouseup');
+let drags = mouseDowns.
+  map(function(e){
+    return parentMouseMoves.
+      takeUntil(parentMouseUps);}).
+  concatAll();
+let subscription = drags.forEach(
+  function(e){
+  widget.style.left = e.clientX + 'px';
+  widget.style.top = e.clientY + 'px';
+}, function onError(error) {
+}, function onCompleted() {
+});
