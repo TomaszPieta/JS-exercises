0f7e6a90f441a9af123a03c1bb6edc566bc55247
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-07-03 21:45:19 +0200

    commit

---
+++ js-dog-api/script.js
@@ -0,0 +1,10 @@
+﻿const doggos = document.querySelector('.doggos');
+
+fetch("https://dog.ceo/api/breeds/image/random")
+.then(res=>res.json())
+.then(res=>{
+  const img = document.createElement('img');
+  img.src = res.message;
+  img.alt = "cute dog";
+  doggos.appendChild(img)
+});
