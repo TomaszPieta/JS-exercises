8fcd6abf7ae77dd089a12e88055447f108b51a3a
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-07-10 12:48:52 +0200

    commit

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 live on Codepen:
 
++ <a href="https://codepen.io/TomaszPieta/pen/YjzZRV">js-bottle-of-wine</a>
 + <a href="https://codepen.io/TomaszPieta/pen/oyWYxV">js-random-query</a>
 + <a href="https://codepen.io/TomaszPieta/pen/eKWRrO">js-color-squares</a>
 + <a href="https://codepen.io/TomaszPieta/pen/zabwaO">js-dog-api</a>
---
+++ js-bottles-of-wine/script.js
@@ -0,0 +1,16 @@
+﻿let num = 9;
+
+while (num >=1) {
+  if (num===2) {
+    let two = `<p>${num} bottles of juice on the wall! ${num} bottles of juice! Take one down, pass it around… ${num-1} bottle of juice on the wall!</p>`;
+    document.write(two);
+  }
+  else if(num===1) {
+    let one = `<p>${num} bottle of juice on the wall! ${num} bottle of juice! Take one down, pass it around… ${num-1} bottles of juice on the wall!</p>`;
+    document.write(one);
+  } else {
+    let other = `<p>${num} bottles of juice on the wall! ${num} bottles of juice! Take one down, pass it around… ${num-1} bottles of juice on the wall!</p>`;
+    document.write(other);
+  }
+num -=1; 
+}
