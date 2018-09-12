94c94ab576724d2946b993dec76f630b037b310d
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-09-12 20:36:36 +0200

    commit

---
+++ js-dinosaurs/script.js
@@ -0,0 +1,8 @@
+﻿let text = "Velociraptor is a  dinosaur.";
+let dinosaur = "Triceratops";
+let dinosaurUppercase = dinosaur.toUpperCase();
+
+let newText = text.replace("Velociraptor", dinosaur);
+
+console.log(newText.length / 2);
+console.log(newText.slice(0, 72));
